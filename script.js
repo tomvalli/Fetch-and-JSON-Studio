d5db13d48a99528874e875dce30b916724e7a8a3
tvalli3 <41927664+tvalli3@users.noreply.github.com>
2020-07-15 00:51:53 -0500

    updated script.js to fetch data from API and parse JSON data into index.html

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // TODO: add code here
+function init() {
+    let container = document.getElementById("container");
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( response=> {
+        response.json().then( json=> {
+            for(let i = 0; i < json.length; i++) {
+                container.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${[json[i].firstName]} ${[json[i].lastName]}</h3>
+                        <ul>
+                            <li>Hours in space: ${[json[i].hoursInSpace]}</li>
+                            <li>Active: ${[json[i].active]}</li>
+                            <li>Skills: ${[json[i].skills.join(", ")]}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${[json[i].picture]}">
+                </div>
+                `;
+            }
+        });
+    });
+}
+
+window.onload = init;
